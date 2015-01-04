d81f7df3248fd6a70ff4fbf248e58a2b9b013bc9
samchandra <samchandra@ebay.com>
2015-01-04 11:18:04 -0800

    1.1

--- Chapter1/src/problems/Problem1.java
+++ Chapter1/src/problems/Problem1.java
@@ -1,5 +1,13 @@
 package problems;
 
+
+/**
+ * Implement an algorithm to determine if a string has all unique characters. 
+ * What if you can not use additional data structures?
+ */
 public class Problem1 {
 
+	public boolean hasUniqueCharacters(String input) {
+		return false;
+	}
 }
---
+++ Chapter1/src/problems/Problem1Test.java
@@ -0,0 +1,38 @@
+package problems;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Implement an algorithm to determine if a string has all unique characters. 
+ * What if you can not use additional data structures?
+ */
+public class Problem1Test {
+
+	protected Problem1 solution;
+	
+	@Before
+    public void setUp() {
+        solution = new Problem1();
+    }
+
+    @Test(timeout = 200)
+    public void testCase0() {
+        String input = "ABCDE";
+        boolean expected = true;
+        boolean actual = solution.hasUniqueCharacters(input);
+        Assert.assertEquals(expected, actual);
+    }
+    
+    @Test(timeout = 200)
+    public void testCase1() {
+        String input = "AABCDE";
+        boolean expected = false;
+        boolean actual = solution.hasUniqueCharacters(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+
+
+}
