6d1b1c974b670cc849bf533457513f625ed85a55
sampriya <monjury@gmail.com>
2015-06-06 20:18:48 -0700

    1.3

--- Chapter1/src/problems/Problem3.java
+++ Chapter1/src/problems/Problem3.java
@@ -6,28 +6,48 @@ import java.util.Map;
 //1.3 Given two strings, write a method to decide if one is a permutation of the other.
 public class Problem3 {
 
-	private Map<Character, Integer> countMap;
-
 	public boolean isPermutation(String str1, String str2) {
-		if (str1.length() != str2.length()) {
+
+		if (!isSameLength(str1, str2)) {
 			return false;
 		}
-		countMap = new HashMap<Character, Integer>();
-		for (int i = 0; i < str1.length(); i++) {
-			setValue(str1.charAt(i), true);
-			setValue(str2.charAt(i), false);
+
+		int len = str1.length();
+		Map<Character, Integer> countMap = new HashMap<Character, Integer>();
+
+		for (int i = 0; i < len; i++) {
+			incrementValue(str1.charAt(i), countMap);
+			decrementValue(str2.charAt(i), countMap);
 		}
-		for(Character c : countMap.keySet()) {
-			if(countMap.get(c) != 0) {
+
+		return isValidMap(countMap);
+	}
+
+	private boolean isValidMap(Map<Character, Integer> countMap) {
+		for (Character c : countMap.keySet()) {
+			if (countMap.get(c) != 0) {
 				return false;
 			}
 		}
 		return true;
 	}
 
-	private void setValue(char key, boolean isFirst) {
-		int value = (countMap.get(key) == null) ? 0 : countMap.get(key);
-		value = isFirst ? value + 1 : value - 1;
-		countMap.put(key, value);
+	private boolean isSameLength(String str1, String str2) {
+		if (str1.length() != str2.length()) {
+			return false;
+		}
+		return true;
+	}
+
+	private void incrementValue(char currentChar, Map<Character, Integer> countMap) {
+		int value = (countMap.get(currentChar) == null) ? 0 : countMap.get(currentChar);
+		value = value + 1;
+		countMap.put(currentChar, value);
+	}
+
+	private void decrementValue(char currentChar, Map<Character, Integer> countMap) {
+		int value = (countMap.get(currentChar) == null) ? 0 : countMap.get(currentChar);
+		value = value - 1;
+		countMap.put(currentChar, value);
 	}
 }
